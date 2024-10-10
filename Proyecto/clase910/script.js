9d4b6bf8e86cf5251343c701cf9f7ec41876335f
Angie Ureche <73198676+Angie3x@users.noreply.github.com>
2024-10-10 06:54:34 -0500

    Prueba de commit

---
+++ Proyecto/clase910/script.js
@@ -0,0 +1,21 @@
+
+
+// let i = 0;
+// // ciclo mientras 
+// while (i <5) {
+//     console.log('Hello World!');
+//     console.log(i);
+//     i++;
+// }
+
+// ciclo do while 
+
+let i = 0;
+
+do {
+   console.log('Hello World!');
+   console.log(i);
+    i++;
+}
+
+while (i < 5);
